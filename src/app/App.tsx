9a9031151b3a2a9ab2bd94695d11e97c8318dc3b
arapbaeva <aiymvector@gmail.com>
2023-04-02 09:01:44 +0600

    added / to gh-pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -60,8 +60,8 @@ function App() {
                 <Routes>
                     <Route path={'/'} element={ <TodolistsList/>}/>
                     <Route path={'/login'} element={<Login/>}/>
-                    {/*<Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>*/}
-                    {/*<Route path={'*'} element={<Navigate to={'/404'}/>}/>*/}
+                    <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'/404'}/>}/>
                 </Routes>
 
 
